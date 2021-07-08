bac6e3d60468965d37245921deaac4e157b92278
Suyeon Lim <84764760+uoahnsuul@users.noreply.github.com>
2021-07-08 11:00:49 +0900

    Add files via upload

---
+++ web/includes/footer.php
@@ -0,0 +1,8 @@
+
+
+  <footer class="py-5">
+        <div class="container">
+
+        </div>
+        <!-- /.container -->
+    </footer>
---
+++ web/includes/header.php
@@ -0,0 +1,29 @@
+﻿    <nav class="navbar fixed-top navbar-toggleable-md bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+              <img src="images/11.png" class="rounded float-left" alt="">&nbsp;
+            </a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php" style="color:#514938"> TAKE</a>
+                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php" style="color:#514938">TAKE 소개</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.php" style="color:#514938">검색&분석</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="today.php" style="color:#514938">오늘의 키워드</a>
+                    </li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
